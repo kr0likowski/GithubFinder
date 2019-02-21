2053e3d66b1ab032e2dc2ccf5591a41be3605346
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2019-02-21 18:25:34 +0100

    Extended functionality

--- js/main.js
+++ js/main.js
@@ -19,9 +19,21 @@ $(document).ready(function(){
   <div class="row">
   <div class="col-md-3">
   <img src="${user.avatar_url}" class="thumbnail avatar"/>
+  <br>
   <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
   </div>
-  <div class="col md-3"></div>
+  <div class="col md-9">
+  <span class="badge badge-pill badge-primary">Public Repos: ${user.public_repos}</span>
+<span class="badge badge-pill badge-secondary">Public Gists: ${user.public_gists}</span>
+<span class="badge badge-pill badge-success">Followers: ${user.followers}</span>
+<span class="badge badge-pill badge-danger">Following: ${user.following}</span></div>
+<br><br>
+<ul class="list-group">
+<li class="list-group-item">Company: ${user.company}</li>
+<li class="list-group-item">Website/blog: ${user.blog}</li>
+<li class="list-group-item">Location: ${user.location}</li>
+<li class="list-group-item">Member since: ${user.created_at}</li>
+</ul>
   </div>
     </div>
 </div>
